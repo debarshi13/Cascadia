2f546fefec24db6f28bdc46a7c04659b66c41f35
Brian Chen <brianyichen@gmail.com>
2024-04-20 12:59:18 -0500

    clickable hexagon

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -16,8 +16,16 @@ public class GamePanel extends JPanel implements MouseListener{
 	private ArrayList<Player> players;
 	private ArrayList<Habitat> unclaimedHabitats;
 	private ArrayList<Habitat> pile1, pile2, pile3, pile4;
+	private Polygon hexagonTest;
 	public GamePanel() {
 		
+		hexagonTest = new Polygon();
+		for (int i = 0; i < 6; i++){
+			hexagonTest.addPoint((int) (150 + 150 * Math.cos(Math.PI/2 + i * 2 * Math.PI / 6)),
+			(int) (150 + 150 * Math.sin(Math.PI/2 + i * 2 * Math.PI / 6)));
+		}
+
+
 		try {
 			background = ImageIO.read(new File("src/images/background.png"));
 		} catch (IOException e) {
@@ -86,14 +94,17 @@ public class GamePanel extends JPanel implements MouseListener{
 		
 		
 		g.drawImage(background, 0, 0, null);
-		System.out.println((players.get(0).getHabitats().get(0).getBiome()));
+		//System.out.println((players.get(0).getHabitats().get(0).getBiome()));
 		g.drawImage(players.get(0).getHabitats().get(0).getImg() , 250, 500, null);
-		System.out.println(pile1);
+
+		//System.out.println(pile1);
 		g.drawImage(pile1.get(pile1.size() - 1).getImg(), 800, 80, null);
 		
-		for(int i = 0; i < pile1.size(); i++) {
-			g.drawImage(pile1.get(pile1.size() - 1).getImg(), 800, 80, null);
-		}
+		
+		g.drawPolygon(hexagonTest);
+		// for(int i = 0; i < pile1.size(); i++) {
+		// 	g.drawImage(pile1.get(pile1.size() - 1).getImg(), 800, 80, null);
+		// }
 		
 		
 		
@@ -101,7 +112,8 @@ public class GamePanel extends JPanel implements MouseListener{
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		System.out.println( "" + e.getX() + "  " + e.getY());
-		
+		if(hexagonTest.contains(e.getPoint()))
+		System.out.println("debarshi is a stinky indian");
 	}
 	@Override
 	public void mousePressed(MouseEvent e) {
@@ -121,6 +133,5 @@ public class GamePanel extends JPanel implements MouseListener{
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
 }

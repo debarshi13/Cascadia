b2de40ba6b40c3188a4218ce7d877932bf86ac29
brianchen831 <brianyichen@gmail.com>
2024-04-21 21:34:57 -0500

    update player

--- src/Player.java
+++ src/Player.java
@@ -1,48 +1,68 @@
+
+import java.awt.Point;
 import java.util.*;
 
 
 public class Player {
-	
-
 	private int natureTokens, numTiles;
 	private Tiles allTiles = new Tiles();
 	private ArrayList<TreeMap<String, Object>> claimedHabitats;
 
-	
 	public Player(int playerNum, int startingTileIdx) {
 		claimedHabitats = new ArrayList<>();
 		
 		Tile[][] startingTiles = allTiles.getStartingTiles();
 
+		Hexagon hex = null;
 		TreeMap<String, Object> habitatInfo = new TreeMap<>();
-		habitatInfo.put("row_idx", 10);
-		habitatInfo.put("col_idx", 10);
+		habitatInfo.put("row_idx", 6);
+		habitatInfo.put("col_idx", 8);
 		habitatInfo.put("habitats", startingTiles[startingTileIdx][0].getHabitats());
 		habitatInfo.put("wildlife", startingTiles[startingTileIdx][0].getWildlife());
 		habitatInfo.put("tokenPlaced", false);
 		habitatInfo.put("rotation", startingTiles[startingTileIdx][0].getRotation());
+		habitatInfo.put("hexagon", hex);
 		claimedHabitats.add(habitatInfo);
+
 		
 		habitatInfo = new TreeMap<>();
-		habitatInfo.put("row_idx", 11);
-		habitatInfo.put("col_idx", 9);
+		habitatInfo.put("row_idx", 7);
+		habitatInfo.put("col_idx", 7);
 		habitatInfo.put("habitats", startingTiles[startingTileIdx][1].getHabitats());
 		habitatInfo.put("wildlife", startingTiles[startingTileIdx][1].getWildlife());
 		habitatInfo.put("tokenPlaced", false);
 		habitatInfo.put("rotation", startingTiles[startingTileIdx][1].getRotation());
+		habitatInfo.put("hexagon", hex);
 		claimedHabitats.add(habitatInfo);
-	
+
+
 		habitatInfo = new TreeMap<>();
-		habitatInfo.put("row_idx", 11);
-		habitatInfo.put("col_idx", 10);
+		habitatInfo.put("row_idx", 7);
+		habitatInfo.put("col_idx", 8);
 		habitatInfo.put("habitats", startingTiles[startingTileIdx][2].getHabitats());
 		habitatInfo.put("wildlife", startingTiles[startingTileIdx][2].getWildlife());
 		habitatInfo.put("tokenPlaced", false);
 		habitatInfo.put("rotation", startingTiles[startingTileIdx][2].getRotation());
+		habitatInfo.put("hexagon", hex);
 		claimedHabitats.add(habitatInfo);
 	}
 
+	public ArrayList<TreeMap<String, Object>> getClaimedHabitats() {
+		return claimedHabitats;
+	}
 
-
-	
+	public TreeMap<String, Object> searchHabitat(Point pt)
+	{
+		
+		for (TreeMap<String, Object> cTile : claimedHabitats) {
+			Hexagon hex = (Hexagon) cTile.get("hexagon");
+			if(hex.contains(pt))
+			{
+				System.out.println("@@@@@@" + cTile.get("habitats"));
+				return cTile;
+			}
+			
+		}
+		return null;
+	}
 }
